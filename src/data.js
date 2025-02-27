989fca8e1e21f261e8a455ccdd5a311c887c0e41
DonnnChaewon <brandonlolz2003.com>
2025-02-27 21:55:45 +0700

    First upload

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const data = [
+  {
+    question: "Which K-Pop idol is originally not from South Korea?",
+    option1: "Wonyoung",
+    option2: "Karina",
+    option3: "Hanni",
+    option4: "Chaewon",
+    answer: 3,
+    explanation: "Hanni was born in Australia with Vietnamese parents, while the other three are originally from South Korea."
+  },
+  {
+    question: "What Indonesian dish is based on beef",
+    option1: "Kerak Telur",
+    option2: "Rendang",
+    option3: "Gulai Ikan",
+    option4: "Ayam Betutu",
+    answer: 2,
+    explanation: "Rendang is a beef that is mixed with spices. Kerak telur is a dish that is made of eggs and rice. While gulai ikan and ayam betutu are similar to fish and chicken with curry, respectively."
+  },
+  {
+    question: "Besides HTML, what programming language that can be used as frontend",
+    option1: "React",
+    option2: "PHP",
+    option3: "Python",
+    option4: "Java",
+    answer: 1,
+    explanation: "React is a programming language mainly for frontend. PHP is basically used for backend which prioritizes function. Python and Java are only for basic programming and data structures."
+  },
+  {
+    question: "If a computer cannot be booted, what component is missing?",
+    option1: "Application Software",
+    option2: "Internet",
+    option3: "Mouse",
+    option4: "Operating System",
+    answer: 4,
+    explanation: "Operating System is a main component of a computer that makes it can be booted. The other three don't affect the booting capability of a computer."
+  },
+  {
+    question: "Which of these colors that is not a primary color?",
+    option1: "Red",
+    option2: "Yellow",
+    option3: "Green",
+    option4: "Blue",
+    answer: 3,
+    explanation: "Green is a mixture of blue and yellow, while the other three are primary colors."
+  },
+  {
+    question: "Which of these characters that is not a Spider-Man villain?",
+    option1: "Vulture",
+    option2: "Sabretooth",
+    option3: "Lizard",
+    option4: "Rhino",
+    answer: 2,
+    explanation: "Sabretooth is a Wolverine villain while the other three are Spider-Man villains."
+  },
+  {
+    question: "What is the capital of England?",
+    option1: "London",
+    option2: "Manchester",
+    option3: "Liverpool",
+    option4: "Birmingham",
+    answer: 1,
+    explanation: "London is the capital of England while the other three are cities in England."
+  },
+  {
+    question: "In games, which element pair counter each other?",
+    option1: "Water and Wind",
+    option2: "Fire and Ice",
+    option3: "Electric and Metal",
+    option4: "Light and Dark",
+    answer: 4,
+    explanation: "In games, light and dark are opposite elements that are also related to real life."
+  },
+];
